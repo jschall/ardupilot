c7c2ad4e841000fb4d72cf7c05d9777a463ffe2e
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2015-10-21 14:27:35 -0700

    AntennaTracker: wire up dummy accel cal for tracker

--- AntennaTracker/Tracker.h
+++ AntennaTracker/Tracker.h
@@ -40,6 +40,7 @@
 #include <AP_Math/AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
 #include <AP_ADC/AP_ADC.h>         // ArduPilot Mega Analog to Digital Converter Library
 #include <AP_InertialSensor/AP_InertialSensor.h> // Inertial Sensor Library
+#include <AP_AccelCal/AP_AccelCal.h>                // interface and maths for accelerometer calibration
 #include <AP_AHRS/AP_AHRS.h>         // ArduPilot Mega DCM Library
 #include <Filter/Filter.h>                     // Filter library
 #include <AP_Buffer/AP_Buffer.h>      // APM FIFO Buffer
@@ -111,6 +112,7 @@ private:
 
     Compass compass;
 
+    AP_AccelCal accelcal;
     AP_InertialSensor ins;
 
     RangeFinder rng {serial_manager};
