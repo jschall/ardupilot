wasanthajp/wasantha

AP_InertialSensor: make primary AP_Accelcal object part of AP_InertialSensor instead of vehicle lib

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -436,12 +436,14 @@ AP_InertialSensor_Backend *AP_InertialSensor::_find_backend(int16_t backend_id)
 }
 
 void
-AP_InertialSensor::init(Sample_rate sample_rate, AP_AccelCal* acal)
+AP_InertialSensor::init(Sample_rate sample_rate)
 {
     // remember the sample rate
     _sample_rate = sample_rate;
-    _acal = acal;
+
+    _acal = new AP_AccelCal;
     _acal->register_client(this);
+
     if (_gyro_count == 0 && _accel_count == 0) {
         _start_backends();
     }

libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -72,7 +72,7 @@ public:
     ///
     /// @param style	The initialisation startup style.
     ///
-    void init(Sample_rate sample_rate,AP_AccelCal *acal);
+    void init(Sample_rate sample_rate);
 
     /// Register a new gyro/accel driver, allocating an instance
     /// number
@@ -231,7 +231,7 @@ public:
     AuxiliaryBus *get_auxiliary_bus(int16_t backend_id);
 
     //Returns accel calibrator interface object pointer
-    AP_AccelCal& get_acal() const { return *_acal; }
+    AP_AccelCal* get_acal() const { return _acal; }
 
     // Returns body fixed accelerometer level data averaged during accel calibration's first step
     bool get_fixed_mount_accel_cal_sample(uint8_t sample_num, Vector3f& ret) const;

jschall/ardupilot

libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -443,13 +443,13 @@ AP_InertialSensor_Backend *AP_InertialSensor::_find_backend(int16_t backend_id, 
 }
 
 void
-AP_InertialSensor::init(uint16_t sample_rate, AP_AccelCal* acal)
+AP_InertialSensor::init(uint16_t sample_rate)
 {
     // remember the sample rate
     _sample_rate = sample_rate;
     _loop_delta_t = 1.0f / sample_rate;
 
-    _acal = acal;
+    _acal = new AP_AccelCal;
     _acal->register_client(this);
 
     if (_gyro_count == 0 && _accel_count == 0) {

libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -63,7 +63,7 @@ public:
     ///
     /// @param style	The initialisation startup style.
     ///
-    void init(uint16_t sample_rate_hz,AP_AccelCal *acal);
+    void init(uint16_t sample_rate_hz);
 
     /// Register a new gyro/accel driver, allocating an instance
     /// number
@@ -228,7 +228,7 @@ public:
     void detect_backends(void);
 
     //Returns accel calibrator interface object pointer
-    AP_AccelCal& get_acal() const { return *_acal; }
+    AP_AccelCal* get_acal() const { return _acal; }
 
     // Returns body fixed accelerometer level data averaged during accel calibration's first step
     bool get_fixed_mount_accel_cal_sample(uint8_t sample_num, Vector3f& ret) const;