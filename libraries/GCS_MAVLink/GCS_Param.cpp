dd2aa6f9ed67cdac18e83579f4afe4b855f374b7
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-02-27 16:38:44 -0800

    GCS_MAVLink: gcs_sendtext warning on bad param read/write

--- libraries/GCS_MAVLink/GCS_Param.cpp
+++ libraries/GCS_MAVLink/GCS_Param.cpp
@@ -246,6 +246,8 @@ void GCS_MAVLINK::handle_param_request_read(const mavlink_message_t &msg)
 
     struct pending_param_request req;
     req.chan = chan;
+    req.sysid = msg.sysid;
+    req.compid = msg.compid;
     req.param_index = packet.param_index;
     memcpy(req.param_name, packet.param_id, MIN(sizeof(packet.param_id), sizeof(req.param_name)));
     req.param_name[AP_MAX_NAME_SIZE] = 0;
@@ -276,6 +278,11 @@ void GCS_MAVLINK::handle_param_set(const mavlink_message_t &msg)
     uint16_t parameter_flags = 0;
     vp = AP_Param::find(key, &var_type, &parameter_flags);
     if (vp == nullptr || isnan(packet.param_value) || isinf(packet.param_value)) {
+        mavlink_msg_command_ack_send(chan, MAVLINK_MSG_ID_PARAM_SET, MAV_RESULT_DENIED,
+                                     0, 0,
+                                     msg.sysid,
+                                     msg.compid);
+        gcs().send_text(MAV_SEVERITY_WARNING, "Param write error (%s)", key);
         return;
     }
 
@@ -398,6 +405,11 @@ void GCS_MAVLINK::param_io_timer(void)
         strncpy(reply.param_name, req.param_name, AP_MAX_NAME_SIZE+1);
         vp = AP_Param::find(req.param_name, &reply.p_type);
         if (vp == nullptr) {
+            mavlink_msg_command_ack_send(chan, MAVLINK_MSG_ID_PARAM_REQUEST_READ, MAV_RESULT_DENIED,
+                                     0, 0,
+                                     req.sysid,
+                                     req.compid);
+            gcs().send_text(MAV_SEVERITY_WARNING, "Param read error (%s)", reply.param_name);
             return;
         }
     }
