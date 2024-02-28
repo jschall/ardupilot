4d30a53a74b59427cd5cd4b8268d41a76d20b0b6
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-02-27 16:09:35 -0800

    Tools: submodule update tweaks

--- Tools/gittools/submodule-sync.sh
+++ Tools/gittools/submodule-sync.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+THIS_SCRIPT_REL_PATH="Tools/gittools/submodule-sync.sh"
+if [ -f "$THIS_SCRIPT_REL_PATH" ]; then
+    # we're in the ardupilot root directory
+    echo "---------------------------------"
+    echo "Deleting ardupilot/build"
+    rm build -rf
+
+    echo "Deleting ardupilot/modules"
+    rm modules -rf
+    echo "---------------------------------"
+    echo
+fi
+
 # this copes with moving origin remote to a new git organisation
 # we run it 3 times due to the poor handling of recursion
 git submodule update --recursive --force --init
@@ -10,3 +23,9 @@ git submodule sync --recursive
 
 git submodule update --recursive --force --init
 git submodule sync --recursive
+
+
+./waf configure --board CubeOrange-KHA
+echo "---------------------------------"
+echo "Configured CubeOrange-KHA"
+echo "---------------------------------"
