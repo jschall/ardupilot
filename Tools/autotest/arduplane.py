d563f5a130cddf53377721f0624f57b6a2d7687c
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-03-07 09:44:09 -0800

    autotest: remove test AerobaticsScripting

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -5531,7 +5531,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.GlideSlopeThresh,
             self.HIGH_LATENCY2,
             self.MidAirDisarmDisallowed,
-            self.AerobaticsScripting,
+            #self.AerobaticsScripting,
             self.MANUAL_CONTROL,
             self.RunMissionScript,
             self.WindEstimates,
