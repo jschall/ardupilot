2f5bbb67d601a84f00260e88be2b04ae00301fcc
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-03-12 13:24:43 -0700

    Plane: disable arming check for Q_ASSIST_SPEED

--- ArduPlane/AP_Arming.cpp
+++ ArduPlane/AP_Arming.cpp
@@ -210,16 +210,6 @@ bool AP_Arming_Plane::quadplane_checks(bool display_failure)
         ret = false;
     }
 
-    /*
-      Q_ASSIST_SPEED really should be enabled for all quadplanes except tailsitters
-     */
-    if (check_enabled(ARMING_CHECK_PARAMETERS) &&
-        is_zero(plane.quadplane.assist_speed) &&
-        !plane.quadplane.tailsitter.enabled()) {
-        check_failed(display_failure,"Q_ASSIST_SPEED is not set");
-        ret = false;
-    }
-
     if ((plane.quadplane.tailsitter.enable > 0) && (plane.quadplane.q_fwd_thr_use != QuadPlane::FwdThrUse::OFF)) {
         check_failed(ARMING_CHECK_PARAMETERS, display_failure, "set Q_FWD_THR_USE to 0");
         ret = false;
