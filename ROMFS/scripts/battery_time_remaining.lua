ae55d6a7befd83197c0db6c4cd8d04ca7cfb60c6
Samuel Tabor <samuel.tabor@krausaerospace.com>
2024-03-12 21:52:37 +0000

    Endurance estimator test 2 (#328)
    * Endurance: Implement battery available fraction
    
    * Account for MPPTs
    
    * lua: Make battery filters scalar not array
    
    * Script: debug
    
    * Init filter at 250W and cleanup
    
    * Adjust initial filtering approach
    
    * Remove debug
    
    * script: Handle case of not enough configured batteries

--- ROMFS/scripts/battery_time_remaining.lua
+++ ROMFS/scripts/battery_time_remaining.lua
@@ -38,19 +38,25 @@ local MAV_CMD_NAV_VTOL_TAKEOFF=84
 local MAV_CMD_NAV_VTOL_LAND=85
 
 local lpf_coef = 0.002
-local power_filtered_w = { }
-local time_remaining_s = { }
+local init_power_estimate = 250
+local power_filtered_w = init_power_estimate
 local SECONDS_MAX = 12*3600 -- 12 hrs
 local SECONDS_MIN = 600 -- 10 mins
 
-local INIT_DELAY_DURATION_MS = 60*1000
+local INIT_DELAY_DURATION_MS = 10*60*1000
 local init_delay_ms = 0
 
 local is_armed_last = false
 local param_to_trigger_announce_STR_NAME = "SCR_USER1"
 local param_to_trigger_announce_prev = 0
 local THIS_SCRIPT_NAME = "Battery Time Remaining"
-
+local battery_capacity_available_frac = 0.7
+local main_bat_idx = 0
+local mppt1_idx = 1
+local mppt2_idx = 2
+local mppt3_idx = 3
+local mppt4_idx = 4
+local is_in_initial_delay = true
 
 -- constrain a value between limits
 function constrain(v, vmin, vmax)
@@ -65,25 +71,34 @@ function constrain(v, vmin, vmax)
  
 function init()
     check_announce(true)
-
-    for i=0, battery:num_instances() do
-        power_filtered_w[i] = 0.0
-        time_remaining_s[i] = 0
-    end
     return update, 1000 -- 1Hz
 end
 
 
-function update_battery_instance_1Hz(instance)
-    if instance >= battery:num_instances() or battery:healthy(instance) == false then
-        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: bail[%d] %d %d', instance, battery:num_instances(), battery:healthy(instance)))
+function update_battery_1Hz()
+
+    if  battery:num_instances() < 5 or battery:healthy(main_bat_idx) == false then
+        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: bail[%d] %d %d', main_bat_idx, battery:num_instances(), battery:healthy(main_bat_idx)))
         return
     end
 
-    local consumed_whr = battery:consumed_wh(instance) -- units W-hr
-    local power_w = battery:current_amps(instance)*battery:voltage(instance) -- units watts
-    
-    local capacity_whr = battery:pack_capacity_mah(instance)*0.0222 -- units W-hr, nominal pack voltage is 22.2
+    local consumed_whr = battery:consumed_wh(main_bat_idx) -- units W-hr
+
+    local voltage = battery:voltage(main_bat_idx)
+
+    -- Total current is the sum of the net current and the MPPTs. Use this as this will be the battery consumption if the solar fails (worst case)
+    local total_curr_a = battery:current_amps(main_bat_idx) + battery:current_amps(mppt1_idx) + battery:current_amps(mppt2_idx) + battery:current_amps(mppt3_idx) + battery:current_amps(mppt4_idx) -- units amps
+    local power_w = total_curr_a * voltage -- units W
+
+    power_w = constrain(power_w, 20, 600)
+
+    local capacity_whr = battery_capacity_available_frac * battery:pack_capacity_mah(main_bat_idx)*0.0222 -- units W-hr, nominal pack voltage is 22.2
+
+    if not total_curr_a then
+        -- sanity check
+        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: current fail'))
+        return
+    end
 
     if not consumed_whr or not power_w or capacity_whr <= 10 then
         -- sanity check
@@ -91,38 +106,37 @@ function update_battery_instance_1Hz(instance)
         return
     end
 
-
     if stop_filtering() then
-        battery:set_time_remaining_external(0, instance)
-        time_remaining_s[instance] = 0
+        battery:set_time_remaining_external(0, main_bat_idx)
+        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: Filter stopped'))
         return
-    elseif time_remaining_s[instance] == 0 then
-        -- first time we are flying, init the LPF
-        power_filtered_w[instance] = power_w
-        -- fast_mode_ms = millis() -- start running in fast mode siliently
     end
     
     -- apply simple 1st order FIR filter to current_amps
     -- This coef is expected to be very very small (like 0.001), to
     -- create a time-constant very very long (like a few minutes)
-    power_filtered_w[instance] = (power_filtered_w[instance] * (1.0-lpf_coef)) + (power_w * lpf_coef)
-    if power_filtered_w[instance] == nil or power_filtered_w[instance] == 0.0 then
+    power_filtered_w = (power_filtered_w * (1.0-lpf_coef)) + (power_w * lpf_coef)
+    if power_filtered_w == nil or power_filtered_w == 0.0 then
         -- divide-by-zero check. Best to just not update it and keep old value
-        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: DBZ'))
+        -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: DBZ [%f %f %f %f]', power_w, power_filtered_w, voltage, total_curr_a))
         return
     end
 
     local whr_remaining = constrain((capacity_whr - consumed_whr), 0, capacity_whr)
     local J_remaining = whr_remaining * 3600
-    time_remaining_s[instance] = math.floor((J_remaining / power_filtered_w[instance]) + 0.5)
+    local time_remaining_s = math.floor((J_remaining / power_filtered_w) + 0.5)
 
-    local time_remaining_s_constrained = constrain(time_remaining_s[instance], SECONDS_MIN, SECONDS_MAX)
+    local time_remaining_s_constrained = constrain(time_remaining_s, SECONDS_MIN, SECONDS_MAX)
 
-    battery:set_time_remaining_external(time_remaining_s_constrained, instance)
+    if is_in_initial_delay then
+        -- During this period we run the averaging but hide the result
+        time_remaining_s_constrained = 0.0
+    end
 
-    -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: batt[%d] = %d, %s', instance+1, time_remaining_s[instance], disp_time(time_remaining_s[instance])))
-    logger:write('EEST','pinst,pfilt,batWhr,estSec','ffff','----','----',power_w, power_filtered_w[instance], whr_remaining, time_remaining_s[instance])
+    battery:set_time_remaining_external(time_remaining_s_constrained, main_bat_idx)
 
+    -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: batt[%d] = %d, %s', main_bat_idx+1, time_remaining_s[main_bat_idx], disp_time(time_remaining_s[main_bat_idx])))
+    logger:write('EEST','pinst,pfilt,batWhr,estSec','ffff','----','----',power_w, power_filtered_w, whr_remaining, time_remaining_s_constrained)
 end
 
 function disp_time(time)
@@ -163,14 +177,24 @@ function stop_filtering()
 
     local now_ms = millis()
     if result then
+        -- Not filtering for one of the reasons above
         init_delay_ms = now_ms
     elseif (init_delay_ms > 0) then
+        -- Above conditions are acceptable for filtering
         if (now_ms - init_delay_ms < INIT_DELAY_DURATION_MS) then
-            -- we're in startup delay, continue to inhibit filtering
+            -- we're in startup delay
             result = true
+            is_in_initial_delay = true
+            -- gcs:send_text(MAV_SEVERITY.DEBUG, string.format('K1000: Startup delay'))
+        elseif (now_ms - init_delay_ms < 2*INIT_DELAY_DURATION_MS) then
+            -- in this period we run the filter but hide the result
+            is_in_initial_delay = true
+            result = false
         else
             -- time has expired
+            is_in_initial_delay = false
             init_delay_ms = 0
+            result = false
         end
     end
     return result
@@ -183,7 +207,7 @@ function update()
     --     update_battery_instance_1Hz(instance)
     -- end
 
-    update_battery_instance_1Hz(0)
+    update_battery_1Hz()
     
     return update, 1000 -- 1Hz
 end
