13f6b2916f7fb94a2c528b48603d4f3dd6085c5e
Samuel Tabor <samuelctabor@gmail.com>
2024-04-22 15:34:56 +0100

    SITL: Output on 14500 for sim

--- Tools/autotest/sim_vehicle.py
+++ Tools/autotest/sim_vehicle.py
@@ -880,7 +880,7 @@ def start_mavproxy(opts, stuff):
 
     for i in instances:
         if not opts.no_extra_ports:
-            ports = [14550 + 10 * i]
+            ports = [14550 + 10 * i, 14500 + 10*i]
             for port in ports:
                 if under_vagrant():
                     # We're running inside of a vagrant guest; forward our
