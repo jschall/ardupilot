5d2dcfef5df0c0832df6cad77d98fa717064378c
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-05-10 15:26:31 -0700

    LUA: update throttle_loss script to set throttle output to SERVOn_MIN if enabled

--- scripts/fw_loss_of_thrust.lua
+++ scripts/fw_loss_of_thrust.lua
@@ -2,7 +2,7 @@
 -- Detects if throttle is above given threshold with
 -- with lower than nominal vibrations
 -- Original: Ryan Beall 20AUG2022
--- Rewritten: Tom Pittenger SEPT2023
+-- Rewritten: Tom Pittenger SEPT2023 and again and again and again
 
 if FWVersion:type() ~= 3 then -- plane
     -- This script is only for plane
@@ -11,22 +11,23 @@ end
 
 local MAV_SEVERITY = {EMERGENCY=0, ALERT=1, CRITICAL=2, ERROR=3, WARNING=4, NOTICE=5, INFO=6, DEBUG=7}
 local param_to_trigger_announce_STR_NAME = "SCR_USER1"
-local param_to_trigger_announce_prev = 0
+local param_to_trigger_announce_prev = param:get(param_to_trigger_announce_STR_NAME)
 local THIS_SCRIPT_NAME = "Deadstick Monitoring"
 local is_armed = false
 
 local K_THROTTLE = 70
 local motor_out_time_ms = 0
-local recovery_start_ms = 0
 local STATE = {INIT=0, NORMAL=1, ESC_RECOVERY=2}
 local current_state = STATE.INIT
-local PARAM_THR_MAX = 0
 local is_vtol = false
 
 -- consider motor stopped when vibe is low and RPM low for more than 4s
 local MOTOR_NOT_HEALTHY_TIMEOUT_MS = 4000
-local ESC_DISABLED_DURATION_MS = 4000
+local ESC_DISABLED_DURATION_MS = 10000
 local ESC_RECOVERY_DURATION_MS = 5000
+
+-- stay in the init state while flying for less than this time
+local STAY_IN_STATE_INIT_UNTIL_FLYING_DURATION_EXCEEDS_MS = 10000
 
 -- vibration threshold below which motor may be stopped
 local VIBE_LOW_THRESH = 1.25
@@ -34,15 +35,9 @@ local VIBE_LOW_THRESH = 1.25
 -- Throttle Threshold % above throttle max which motor should be considered valid to check vibes
 local THROTTLE_ON_THRESH_PCT = 90
 
-local THRUST_LOSS_ACTION_WARN_ONLY = true
 local MOTOR_FAIL_WARN_SNOOZE_DURATION_MS = 5000
 
--- local is_SITL = false
-local sitl_check_value = param:get('SIM_SPEEDUP')
-if (sitl_check_value ~= nil) then
-    -- is_SITL = true
-    VIBE_LOW_THRESH = 0.75
-end
+local is_SITL = (param:get('SIM_SPEEDUP') ~= nil)
 
 function update()
 
@@ -54,28 +49,24 @@ function update()
     end
 
     if is_vtol then
+        -- this script is disabled for vtol aircraft
         return update, 1000
     end
 
     -- assume 1Hz default update interval
     local sleep_duration_ms = 1000
 
-    if not is_armed then
+    local flight_duration = vehicle:get_time_flying_ms()
+    if not is_armed or flight_duration < STAY_IN_STATE_INIT_UNTIL_FLYING_DURATION_EXCEEDS_MS then
         current_state = STATE.INIT
     end
 
     if current_state == STATE.INIT then
         motor_out_time_ms = 0
-        recovery_start_ms = 0
 
-        if is_armed then
-            -- we either just armed or have just recovered and are already flying
+        if flight_duration >= STAY_IN_STATE_INIT_UNTIL_FLYING_DURATION_EXCEEDS_MS then
+            -- we either just performed a takeoff or an ESC recovery and are already flying
             current_state = STATE.NORMAL
-
-            if just_armed then
-                -- Takeoff or a Motor test. Don't check again for several seconds to revent a false positive
-                sleep_duration_ms = 5000
-            end
         end
 
     elseif current_state == STATE.NORMAL then
@@ -89,14 +80,19 @@ function update()
                 -- first time we've seen a problem, timestamp it
                 motor_out_time_ms = millis()
             elseif millis() - motor_out_time_ms > MOTOR_NOT_HEALTHY_TIMEOUT_MS then
-                if THRUST_LOSS_ACTION_WARN_ONLY then
-                    gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Loss of Thrust Detected")
+                gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Loss of Thrust Detected")
+
+                local param_thr_loss_fix = param:get("KHA_THR_LOSS_FIX")
+                if param_thr_loss_fix == nil or param_thr_loss_fix == 0 then
                     sleep_duration_ms = MOTOR_FAIL_WARN_SNOOZE_DURATION_MS
                     current_state = STATE.INIT
                 else
-                    gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Loss of Thrust Detected, Disabling Motor")
+                    gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Resetting propulsion system")
                     -- disabling motor and sleeping for a while. When we wake up, we'll recover the ESC
-                    param:set("THR_MAX", 0)
+                    local throttle_chan = SRV_Channels:find_channel(K_THROTTLE)
+                    local servo_min = param:get("SERVO" .. throttle_chan + 1 .. "_MIN")
+                    SRV_Channels:set_output_pwm_chan_timeout(throttle_chan, servo_min, ESC_DISABLED_DURATION_MS)
+       
                     sleep_duration_ms = ESC_DISABLED_DURATION_MS -- sleep for a few seconds with THR_MAX=0
                     current_state = STATE.ESC_RECOVERY
                 end
@@ -105,22 +101,9 @@ function update()
 
     elseif current_state == STATE.ESC_RECOVERY then
         -- enable motor gradually by raising THR_MAX over ESC_RECOVERY_DURATION_MS in time steps of sleep_duration_ms
-        sleep_duration_ms = 500
-        if (recovery_start_ms == 0) then
-            gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Motor Re-Enabled")
-            recovery_start_ms = millis()
-        else
-            -- millis() returns uint32_t so we need to convert to float to become a number which param:set() needs
-            local dt_ms = (millis() - recovery_start_ms):tofloat()
-            local duration_progress = (dt_ms / ESC_RECOVERY_DURATION_MS)
-            local throttle = PARAM_THR_MAX * duration_progress
-            if (throttle >= PARAM_THR_MAX) then
-                throttle = PARAM_THR_MAX
-                current_state = STATE.INIT
-            end
-            param:set("THR_MAX", throttle)
-        end
-
+        sleep_duration_ms = ESC_RECOVERY_DURATION_MS
+        gcs:send_text(MAV_SEVERITY.EMERGENCY, "K1000: Propulsion system as been reset")
+        current_state = STATE.INIT
     else
         -- default case. This should never happen
         current_state = STATE.INIT
@@ -131,17 +114,16 @@ end
 
 function is_motor_healthy()
     local thr_max = param:get("THR_MAX")
-    if thr_max < 10 then
+    if thr_max < 10 and not is_SITL then
         -- don't accidently store an abnormally low THR_MAX value.
         -- This should never happen but if it does then at "full throttle"
         -- we won't be able to detect it anyway
         return true
     end
 
-    PARAM_THR_MAX = thr_max
     local throttle = SRV_Channels:get_output_scaled(K_THROTTLE)
     local vibe = ahrs:get_vibration():length()
-    local throttle_on_thresh = PARAM_THR_MAX * (THROTTLE_ON_THRESH_PCT * 0.01)
+    local throttle_on_thresh = thr_max * (THROTTLE_ON_THRESH_PCT * 0.01)
     return (vibe > VIBE_LOW_THRESH) or (throttle < throttle_on_thresh)
 end
 
@@ -173,11 +155,8 @@ function init()
         is_vtol = false
     end
 
-    param_to_trigger_announce_prev = param:get(param_to_trigger_announce_STR_NAME)
     announce()
     
-    -- sanity check that this param is available. Lets assert here instead of script start so we can see the error easier in the GCS
-    PARAM_THR_MAX = assert(param:get("THR_MAX"),"K1000: Could not read THR_MAX")
     return update, 1000
 end
 
